29b180194854ab61adad5aa2c740a2c59583d572
Shamkhal <zeynalshamkhal@gmail.com>
2021-03-13 16:30:49 -0800

    String Methods

---
+++ StringMethods/DotnetFirst/IString.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace DotnetFirst
+{
+    class IString
+    {
+    
+
+    public int ILenght(string word)
+    {
+        int i = 0;
+        foreach (char c in word)
+        {
+            i++;
+        }
+        return i;
+    }
+
+    public bool IContains(string word, string secondWord)
+    {
+        for (int i = 0; i < ILenght(word) - ILenght(secondWord) + 1; i++)
+        {
+            if (word.Substring(i, ILenght(secondWord)) == secondWord)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    public bool IEndWith(string word, string secondWord)
+    {
+        if (word.Substring(ILenght(word) - ILenght(secondWord), ILenght(secondWord)) == secondWord)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public int IIndexOf(string word, string secondWord)
+    {
+        int i = 0;
+
+        while (i < ILenght(word) - ILenght(secondWord))
+        {
+            if (word.Substring(i, ILenght(secondWord)) == secondWord)
+            {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    } 
+    public int ILastIndexOf(string word, string secondWord)
+    {
+        int i = word.Length;
+
+        while (i > secondWord.Length)
+        {
+            if (word.Substring(i - ILenght(secondWord), ILenght(secondWord)) == secondWord)
+            {
+                return i - ILenght(secondWord);
+            }
+            i--;
+        }
+
+        return -1;
+
+    }
+    
+        
+    }
+}
---
+++ StringMethods/DotnetFirst/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using DotnetFirst;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "shamkhal";
+
+        //Console.WriteLine(str.Contains("sh"));
+        
+
+        IString Istr = new IString();
+
+        //Console.Write(Istr.ILenght(""));
+        // Console.Write(Istr.IContains("shamkhal", "sh"));
+        //Console.Write(Istr.IEndWith("shamkhal", "all"));
+        //Console.Write(Istr.ILastIndexOf("shamkhal", "a"));
+
+        //Console.Write(str.IndexOf("aml"));
+        
+        Console.Write(String.Empty);
+       
+        
+        
+        //Console.ReadKey();
+    }
+
+    
+}
+    
